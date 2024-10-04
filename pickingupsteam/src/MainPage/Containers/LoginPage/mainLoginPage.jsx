fa99f083009fd764ab5021054137432b1263c685
mylo-yu-VFX2 <113409346+docmcstuffins33@users.noreply.github.com>
2024-10-03 17:23:45 -0700

    Changed Firebase functions and completed Signup
    Changed firebase Fetch/Write user functions to just take in user ID and user data for clarity. Also now adds an entry to firestore on signup corresponding to account.

--- pickingupsteam/src/MainPage/Containers/LoginPage/mainLoginPage.jsx
+++ pickingupsteam/src/MainPage/Containers/LoginPage/mainLoginPage.jsx
@@ -1,19 +1,36 @@
 import React, {useState} from 'react'
 import './MainLoginPage.css'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../../../Firebase/Firebase'
+import { auth, db } from '../../../Firebase/Firebase'
+import { fetchUser, writeUser } from '../../../Firebase/FirebaseUtils'
 
 const MainLoginPage = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [steamID, setSteamID] = useState("");
+  const [username, setUsername] = useState("");
 
   const handleRegister = async (e) => {
     e.preventDefault()
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       const user = auth.currentUser;
+      if(user) {
+        const userData = {
+          Username: username,
+          Email: email,
+          Points: 0,
+          SteamID: steamID,
+          Games: [],
+          Inventory: {
+            Banners: [],
+            Icons: []
+          }
+        }
+        console.log(userData);
+        await writeUser(user.uid, userData);
+      }
       console.log(user);
     } catch (error) {
       console.log(error);
@@ -26,12 +43,10 @@ const MainLoginPage = () => {
     <form>
       <h1>Log In</h1>
       <div>
-        <input type="email" class="form-control" name="email" required></input>
-        <label for="email">Email address</label>
+        <input type="email" name="email" required></input>
       </div>
       <div>
-        <input type="password" class="form-control" name="password" required></input>
-        <label for="password">Password</label>
+        <input type="password"name="password" required></input>
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -46,7 +61,13 @@ const MainLoginPage = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         name="email" required></input>
-        <label for="email">Email address</label>
+      </div>
+      <div>
+        <input type="text"             
+        placeholder='Enter Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        name="username" required></input>
       </div>
       <div>
         <input type="text"        
@@ -54,7 +75,6 @@ const MainLoginPage = () => {
         value={steamID}
         onChange={(e) => setSteamID(e.target.value)}
         name="steamid" required></input>
-        <label for="steamid">Steam ID</label>
       </div>
       <div>
         <input type="password" 
@@ -62,7 +82,6 @@ const MainLoginPage = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         name="password" required></input>
-        <label for="password">Password</label>
       </div>
       <button type="submit">Submit</button>
     </form>
