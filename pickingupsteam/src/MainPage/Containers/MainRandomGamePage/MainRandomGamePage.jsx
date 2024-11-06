b5a2344dabb41470494102f57ee135a4291f7473
Fraser Copland <frasercop@gmail.com>
2024-11-05 18:22:09 -0800

    Added checkboxes

--- pickingupsteam/src/MainPage/Containers/MainRandomGamePage/MainRandomGamePage.jsx
+++ pickingupsteam/src/MainPage/Containers/MainRandomGamePage/MainRandomGamePage.jsx
@@ -25,6 +25,42 @@ const MainRandomGamePage = () => {
     // Random Game wheel Data
     const [randomGame, setRandomGame] = useState(null);
 
+    //Filter Data
+    const [selectedfilters, setSelectedFilters] = useState([]);
+    const [filteredList, setFilteredList] = useState(gameData);
+    
+    var filters = ["Indie", "Casual", "Action", "Adventure", "RPG", "Action-Adventure", "Racing", "Simulation", "Strategy", "Sports", "Software"]
+
+    //checkbox stuff
+    const [checked, setChecked] = useState({'Indie': false, 'Casual': false, 'Action': false, 'Adventure': false, 'RPG': false, 'Action-Adventure': false, 'Racing': false, 'Simulation': false, 'Strategy': false, 'Sports': false, 'Software': false});
+    
+    const handleChange = (genreChosen) => {
+        console.log(genreChosen);
+        setChecked((previousState) => ({
+            ...previousState,
+            [genreChosen]: !previousState[genreChosen]
+        }));
+        console.log(checked[genreChosen]);
+
+        // setChecked(checked);
+        // if (selectedfilters.findIndex({genreChosen}) == -1) {
+        //     selectedfilters.push({genreChosen});
+        //     setSelectedFilters(selectedfilters);
+        // } else {
+        //     selectedfilters.splice(selectedfilters.findIndex({genreChosen}),1);
+        //     setSelectedFilters(selectedfilters);
+        // }
+    };
+
+    const Checkbox = ({ label, value, onChange }) => {
+        return (
+          <label>
+            <input type="checkbox" checked={value} onChange={onChange} name={label}/>
+            {label}
+          </label>
+        );
+      };
+
     useEffect(() => {
         if (user) {
             fetchGameData(user.SteamID);
@@ -63,16 +99,16 @@ const MainRandomGamePage = () => {
     }
 
     return (
-    <div class="app__main_container">
-        <div class="app__searchBar">
+    <div className="app__main_container">
+        <div className="app__searchBar">
             {user && user.Inventory &&user.Inventory.Icons ? 
-            <div class ="app_user-profile-container">
+            <div className ="app_user-profile-container">
                 <img src={user.Inventory.Icons[0]?.img} alt={`${user.Username}'s icon`} className="app__user-profile-iconTheme" />
-                <h1 class="app__user-profile-text">Welcome, {user.Username}!</h1>
+                <h1 className="app__user-profile-text">Welcome, {user.Username}!</h1>
             </div>
             :
             <>
-                <input class="app__searchBar-input"
+                <input className="app__searchBar-input"
                 type="text"
                 placeholder='Enter Steam User ID'
                 value={steamID}
@@ -85,6 +121,15 @@ const MainRandomGamePage = () => {
             }
 
         </div>
+        <div className='app__filterPanel'>
+            <form className='app__filterListForm'>
+                    {filters.map((genre, idx) => (
+                    <Checkbox key={genre} label={genre} value={checked[genre]} onChange={(event)=> {handleChange(genre)}}/>
+                ))}
+            </form>
+            
+        </div>
+
         <div className="app__gameListPanel">
             <div className="app__gameList">
                 <h2>Game List </h2>
