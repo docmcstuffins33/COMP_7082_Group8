5d2484dface872a0d4006764e32db7d7ba30ddf4
Andrew L. <andrewliang0123@gmail.com>
2024-11-23 23:50:48 -0800

    added url without port from steam api server

--- pickingupsteam/src/SteamUtils/SteamAccount.js
+++ pickingupsteam/src/SteamUtils/SteamAccount.js
@@ -11,7 +11,17 @@ async function GetGameByUserID(userID, serverURL, serverPort){
         // #########     IF YOU DO NOT, THE WEBSITE WILL SIMPLY NOT LOAD     ###########
         // Also ideally there should be some way to dynamically change the user id sent here. Do this later!
         //const response = await axios.get('http://localhost:8080/api/gamesByUser/' + userID);
-        const response = await axios.get(`http://${serverURL}:${serverPort}/api/gamesByUser/${userID}}`);
+        let response;
+
+        if(!serverPort || serverPort === null){
+            // this is for deployment, in real case this should be the url of the server
+            console.log("--------------------No port provided---------------------");
+            response = await axios.get(`${serverURL}/api/gamesByUser/${userID}`);
+        }
+        else{
+            response = await axios.get(`http://${serverURL}:${serverPort}/api/gamesByUser/${userID}}`);
+        }
+        
         console.log(response.data)
         const filteredList = response.data.applist.apps.filter(x => x.name)
         return filteredList;
