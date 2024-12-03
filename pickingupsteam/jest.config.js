3c7c16ecaa5ef0f8cc4301ec9fef4a59aeee80e1
Andrew L. <andrewliang0123@gmail.com>
2024-12-02 17:50:50 -0800

    Update jest.config.js

--- pickingupsteam/jest.config.js
+++ pickingupsteam/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-    preset: 'react',
     transform: {
       '^.+\\.[t|j]sx?$': 'babel-jest', // Ensure JSX and JS files are processed
     },
-    transformIgnorePatterns: [
-      '/node_modules/(?!your-esm-module-to-transform)/', // In case you're using ES modules
-    ],
   };
