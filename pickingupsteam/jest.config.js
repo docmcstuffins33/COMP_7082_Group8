9f5ced2165048e878d0cb44c13a26856a3411944
Andrew L. <andrewliang0123@gmail.com>
2024-12-02 17:46:54 -0800

    update jest config to react preset

--- pickingupsteam/jest.config.js
+++ pickingupsteam/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    setupFiles: ['<rootDir>/src/setupTests.js'],
+    preset: 'react',
+    transform: {
+      '^.+\\.[t|j]sx?$': 'babel-jest', // Ensure JSX and JS files are processed
+    },
+    transformIgnorePatterns: [
+      '/node_modules/(?!your-esm-module-to-transform)/', // In case you're using ES modules
+    ],
   };
